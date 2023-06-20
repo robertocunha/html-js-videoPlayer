154cffb18c722107ac42edd1c3d7a3aa2fc77814
Roberto Cunha <robertocunha@gmail.com>
2023-06-19 23:39:29 -0300

    Load video sources via JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(42);
+const videoPlayer = document.getElementById('videoPlayerId');
+
+const sources = [
+  { src: 'videos/video01.ogg', type: 'video/ogg' },
+  { src: 'videos/video01.mp4', type: 'video/mp4' }
+];
+
+for (const source of sources) {
+  const sourceElement = document.createElement('source');
+  sourceElement.src = source.src;
+  sourceElement.type = source.type;
+  
+  videoPlayer.appendChild(sourceElement);
+}
+
+videoPlayer.load();
+
