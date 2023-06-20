c7c3884ad4eb08a5d90d02624a5172d3fe16c6e6
Roberto Cunha <robertocunha@gmail.com>
2023-06-20 01:26:54 -0300

    Change video source via JS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const videoPlayer = document.getElementById('videoPlayerId');
+const sourceButtons = document.querySelectorAll('button');
 
-const sourceInfo = [
-  { src: 'videos/video01.ogg', type: 'video/ogg' },
-  { src: 'videos/video01.mp4', type: 'video/mp4' }
+const sources = [
+    {
+        formats: [
+            { src: 'videos/video01.ogg', type: 'video/ogg' },
+            { src: 'videos/video01.mp4', type: 'video/mp4' }
+        ]
+    },
+    {
+        formats: [
+            { src: 'videos/video02.ogg', type: 'video/ogg' },
+            { src: 'videos/video02.mp4', type: 'video/mp4' }
+        ]
+    }
+  
 ];
 
-for (const info of sourceInfo) {
+for (const info of sources[0].formats) {
   const sourceElement = document.createElement('source');
   sourceElement.src = info.src;
   sourceElement.type = info.type;
@@ -15,3 +27,18 @@ for (const info of sourceInfo) {
 
 videoPlayer.load();
 
+sourceButtons.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        videoPlayer.innerHTML = ''; // Limpar elementos <source> existentes
+        
+        for (const info of sources[index].formats) {
+            const sourceElement = document.createElement('source');
+            sourceElement.src = info.src;
+            sourceElement.type = info.type;
+            
+            videoPlayer.appendChild(sourceElement);
+        }
+          
+        videoPlayer.load();
+    })
+});
